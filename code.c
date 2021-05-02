a14261b55ce191e89e159e5f20224c47b9acafc9
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2021-05-02 22:10:13 +0700

    Update code.c

--- code.c
+++ code.c
@@ -1,97 +1,80 @@
-/*
- * Mantas Utka 
-*/
-
-int ledPin1= 13; //Adding the LED's and sensor.
-int ledPin2= 12;
-int ledPin3= 11;
-int ledPin4= 10;
-int ledPin5= 9;
-int ledPin6= 8;
-int ledPin7= 7;
-int ledPin8= 6;
-int sensorPin= A0; //The sensor selecting as Analog pin. The Analog Pin is better for projects as this one.
-int val = 0;
-
-void setup(){
-  pinMode(ledPin1, OUTPUT); //Selecting LEDs as outputs to push the results in LEDs of sound detector
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
-  pinMode(ledPin5, OUTPUT);
-  pinMode(ledPin6, OUTPUT);
-  pinMode(ledPin7, OUTPUT);
-  pinMode(ledPin8, OUTPUT);
-  pinMode(sensorPin, INPUT); //Selecting the sensor as input to send results into Arduino
-  Serial.begin (9600);
+int sensorSuara = A0;
+int pinLED1 = 13;
+int pinLED2 = 12;
+int pinLED3 = 11;
+int pinLED4 = 10;
+int pinLED5 = 9;
+int pinLED6 = 8;
+int pinLED7 = 7;
+int pinLED8 = 6;
+void setup() {
+  Serial.begin(9600);
+  pinMode (pinLED1, OUTPUT);
+  pinMode (pinLED2, OUTPUT);
+  pinMode (pinLED3, OUTPUT);
+  pinMode (pinLED4, OUTPUT);
+  pinMode (pinLED5, OUTPUT);
+  pinMode (pinLED6, OUTPUT);
+  pinMode (pinLED7, OUTPUT);
+  pinMode (pinLED8, OUTPUT);
+  pinMode (sensorSuara, INPUT) ;
 }
-  
-void loop (){
-  val =analogRead(sensorPin);
-  Serial.println (val);
-  // when the sensor detects a signal, LED flashes
-  
-  //1
-  if (val >= 127) {
-    digitalWrite(ledPin1, HIGH); //selecting the values of LEDs to flash by detecting values of sound sensor.
+void loop () {
+  int sensorValue = digitalRead(sensorSuara);
+  //  1
+  if (sensorValue == HIGH) {
+    digitalWrite (pinLED1, HIGH);
   }
   else {
-    digitalWrite(ledPin1, LOW);
+    digitalWrite (pinLED1, LOW);
   }
-
-  //2
-   if (val >= 378) {
-    digitalWrite(ledPin2, HIGH);
-  }
-  else {
-    digitalWrite(ledPin2, LOW);
-  }
-
-   //3
-   if (val >= 505) {
-    digitalWrite(ledPin3, HIGH);
+  // 2
+  if (sensorValue == LOW) {
+    digitalWrite (pinLED2, LOW);
   }
   else {
-    digitalWrite(ledPin3, LOW);
+    digitalWrite (pinLED2, HIGH);
   }
-
-  //4
-  if (val >= 632) {
-    digitalWrite(ledPin4, HIGH);
-  }
-  else {
-    digitalWrite(ledPin4, LOW);
-  }
-
-  //5
-  if (val >= 759) {
-    digitalWrite(ledPin5, HIGH);
+  // 3
+  if (sensorValue == HIGH) {
+    digitalWrite (pinLED3, HIGH);
   }
   else {
-    digitalWrite(ledPin5, LOW);
+    digitalWrite (pinLED3, LOW);
   }
-
-  //6
-  if (val >= 886) {
-    digitalWrite(ledPin6, HIGH);
-  }
-  else {
-    digitalWrite(ledPin6, LOW);
-  }
-
-  //7
-  if (val >= 980) {
-    digitalWrite(ledPin7, HIGH);
+  // 4
+  if (sensorValue == LOW) {
+    digitalWrite (pinLED4, LOW);
   }
   else {
-    digitalWrite(ledPin7, LOW);
+    digitalWrite (pinLED4, HIGH);
   }
-
-  //8
-  if (val >= 1000) {
-    digitalWrite(ledPin8, HIGH);
+  // 5
+  if (sensorValue == HIGH) {
+    digitalWrite (pinLED5, HIGH);
   }
   else {
-    digitalWrite(ledPin8, LOW);
+    digitalWrite (pinLED5, LOW);
+  }
+  // 6
+  if (sensorValue == LOW) {
+    digitalWrite (pinLED6, LOW);
+  }
+  else {
+    digitalWrite (pinLED6, LOW);
+  }
+  // 7
+  if (sensorValue == HIGH) {
+    digitalWrite (pinLED7, HIGH);
+  }
+  else {
+    digitalWrite (pinLED7, LOW);
+  }
+  // 8
+  if (sensorValue == LOW) {
+    digitalWrite (pinLED8, LOW);
+  }
+  else {
+    digitalWrite (pinLED8, LOW);
   }
 }
